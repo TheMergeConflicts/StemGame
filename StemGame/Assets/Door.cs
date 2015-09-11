9e38328a2d3e23e0f8612f1fff28d2c10b519b84
Peilin Li <lipeilin@gatech.edu>
2015-09-10 20:46:14 -0400

    Implemented door and pressure plate.

---
+++ StemGame/Assets/Door.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Door : MonoBehaviour {
+
+	private SpriteRenderer spriteR;
+	private Collider2D col;
+	public bool open = false;
+
+
+	// Use this for initialization
+	void Start () {
+		spriteR = GetComponent<SpriteRenderer> ();
+		col = GetComponent<Collider2D> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (open) {
+			col.enabled = false;
+			spriteR.color = Color.green;
+		} 
+		else {
+			col.enabled = true;
+			spriteR.color = Color.red;
+		}
+	}
+}
---
+++ StemGame/Assets/pressurePlate.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class pressurePlate : MonoBehaviour {
+
+	private SpriteRenderer spriteR;
+	public Door door;
+
+	// Use this for initialization
+	void Start () {
+		spriteR = GetComponent<SpriteRenderer> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter2D(Collider2D other){
+		Debug.Log(123);
+		if(other.gameObject.name == "PlayerCube"){
+			openDoor();
+
+		}
+
+	}
+
+	void openDoor(){
+		door.open = true;
+		spriteR.color = Color.green;
+	}
+}
