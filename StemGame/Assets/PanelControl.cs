339c4987b8e07fd416888cafd11867259aa0c2c9
gstache <gstache2@gmail.com>
2015-09-22 12:21:42 -0400

    Temperature UI
    Added a Temperature_Manager prefab, with a script that has a getTemp()
    and setTemp(float temp) methods. Created a canvas with thermometer
    textures, and a panelControl script that sets the text (to display temp)
    and the amount of the thermometer filled by the panel. The thermometer
    goes from 0-1000k

---
+++ StemGame/Assets/PanelControl.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class PanelControl : MonoBehaviour {
+	private TempManager man;
+	RectTransform trans;
+	public GameObject target;
+	public Text text;
+	// Use this for initialization
+	void Start () {
+		man = target.GetComponent<TempManager> ();
+		trans = GetComponent<RectTransform> ();
+		trans.sizeDelta = new Vector2 (trans.sizeDelta.x, 1);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float scale = 127.31f;
+		if(man){
+			float temp = man.getTemp();
+			text.text = temp.ToString() + "K";
+			float height = scale * temp / 1000f;
+			trans.sizeDelta = new Vector2 (trans.sizeDelta.x, height);
+		}
+	}
+}
---
+++ StemGame/Assets/Scripts/TempManager.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TempManager : MonoBehaviour {
+	private float temp;
+
+	// Use this for initialization
+	void Start () {
+		temp = 273.15f;
+
+	}
+	
+	// Update is called once per frame
+	public float getTemp(){
+		return temp;
+	}
+
+	public void setTemp(float temp){
+		temp = temp;
+	}
+}
