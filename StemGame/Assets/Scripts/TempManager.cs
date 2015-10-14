0fe72aeaa535d7fb45b414de177f6af0dd23ec95
ryanAndersen05 <ryan.andersen05@gmail.com>
2015-10-14 15:14:00 -0400

    Heat shader variables set

--- StemGame/Assets/Scripts/TempManager.cs
+++ StemGame/Assets/Scripts/TempManager.cs
@@ -6,12 +6,20 @@ public class TempManager : MonoBehaviour {
 	public float temp;
     public float tempSmoothing = 1;
 	public Slider temperatureSlider;
+    public ParticleSystem heatDistort;
 	// Use this for initialization
 	void Start () {
 
 	}
 	void Update(){
 		temp = temperatureSlider.value;
+        if(temp > 300.0f)
+        {
+            heatDistort.enableEmission = true;
+        } else
+        {
+            heatDistort.enableEmission = false;
+        }
 	}
 	// Update is called once per frame
 	public float getTemp(){
