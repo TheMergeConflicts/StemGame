9058124b76e86ef5e630b597a5c044ca32bd9cfd
Nick <popescu.nicolai@yahoo.com>
2015-10-19 03:21:16 -0400

    Added Ice Screen Shader
    It's the Ice Age, yall!

--- StemGame/Assets/Scripts/TempManager.cs
+++ StemGame/Assets/Scripts/TempManager.cs
@@ -7,12 +7,29 @@ public class TempManager : MonoBehaviour {
     public float tempSmoothing = 1;
 	public Slider temperatureSlider;
     public ParticleSystem heatDistort;
+	public GameObject iceDistorter;
+	public GameObject iceTexture;
 	// Use this for initialization
 	void Start () {
 
 	}
 	void Update(){
 		temp = temperatureSlider.value;
+		float iceStrength = -.1f * (temp-100);
+		float iceColorStrength = (.3f) * (1-(temp / 100f));
+		Color iceColor = iceTexture.GetComponent<Renderer> ().material.color;
+
+
+
+		if (temp < 100.0f) {
+			iceDistorter.GetComponent<Renderer> ().material.SetFloat ("_Refraction", iceStrength);
+			Color iceModifiedColor = new Color (iceColor.r, iceColor.g, iceColor.b, iceColorStrength);
+			iceTexture.GetComponent<Renderer> ().material.color = iceModifiedColor;
+		} else { // to make sure screen ice is completely gone if we skip the melting stage
+			iceDistorter.GetComponent<Renderer> ().material.SetFloat ("_Refraction", 0f);
+			Color iceModifiedColor = new Color (iceColor.r, iceColor.g, iceColor.b, 0f);
+			iceTexture.GetComponent<Renderer> ().material.color = iceModifiedColor;
+		}
         if(temp > 300.0f)
         {
             heatDistort.enableEmission = true;
