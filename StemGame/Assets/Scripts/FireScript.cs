4e2f1cbd4f326a43b4c47fd289282dec08d14b73
gstache <gstache2@gmail.com>
2015-10-22 10:42:21 -0400

    Created "Flamethrower" prefab
    Just set the lement needed variable and BAM: flames

---
+++ StemGame/Assets/Scripts/FireScript.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FireScript : MonoBehaviour {
+    int animTime = 1;
+    float startTime;
+    SpriteRenderer render;
+	void Start () {
+        render = GetComponent<SpriteRenderer>();
+
+	}
+	
+    void Update()
+    {
+        if(render.enabled)
+        {
+          if (Time.time - startTime > animTime)
+            {
+                render.enabled = false;
+            }
+        }
+    }
+
+	public void fire()
+    {
+        render.enabled = true;
+        startTime = Time.time;
+    }
+}
---
+++ StemGame/Assets/Scripts/fireStarterPlate.cs
@@ -0,0 +1,88 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class fireStarterPlate : MonoBehaviour {
+	
+	
+	//public GameObject panel;
+	private Text text;
+	public string elementNeeded;
+	public float offsetX;
+	public float offsetY;
+	
+	
+	private SpriteRenderer spriteR;
+	public GameObject[] affectedGameObjects;
+	FireScript[] affectedObjects;
+	private int numAffectedObjects;
+	private bool plateOccupied;
+	private Camera cam;
+	
+	// Use this for initialization
+	void Start () {
+		cam = Camera.main;
+		//text = panel.transform.Find ("Text").gameObject.GetComponent<Text> ();
+		plateOccupied = false;
+		spriteR = GetComponent<SpriteRenderer> ();
+		//door = doorObject.GetComponent<Door> ();
+
+		numAffectedObjects = affectedGameObjects.Length;
+		affectedObjects = new FireScript[numAffectedObjects];
+
+		for (int i = 0; i < numAffectedObjects; i++) {
+			affectedObjects[i] = affectedGameObjects[i].GetComponent<FireScript>();
+		}
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(plateOccupied){
+			//panel.SetActive(true);
+		}
+		else{
+			//Debug.Log("false");
+			//panel.SetActive(false);
+		}
+	}
+	
+	void OnTriggerEnter2D(Collider2D other){
+		
+		if (other.gameObject.tag == "Grabbable") {
+			Debug.Log("NICK");
+			plateOccupied = true;
+			if (other.gameObject.name.Contains(elementNeeded)) { 
+				Debug.Log("right object");
+				performAction();
+                Destroy(other.gameObject);
+				
+			} 
+		}
+	}
+	
+
+	
+	void OnTriggerExit2D(Collider2D other){
+		if (other.gameObject.tag == "Grabbable") {
+			plateOccupied = false;
+		}
+	}
+	
+	void performAction(){
+		for (int i = 0; i < numAffectedObjects; i++) {
+			affectedObjects[i].fire();
+		}
+		
+	}
+	
+	void OnDrawGizmos(){
+		//if (affectedGameObjects != null) {
+			Gizmos.color = Color.red;
+			for (int i = 0; i < numAffectedObjects; i++) {
+				Gizmos.DrawLine (gameObject.transform.position, affectedGameObjects[i].transform.position);
+			}
+
+		//}
+	}
+}
