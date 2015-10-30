7b5cf01773f61f019edcd62552fbbd24c761bcb7
nshooter3 <nick.shooter@gmail.com>
2015-10-30 14:42:02 -0400

    Revert "Added player sounds, pit fill sounds"
    This reverts commit e918d21f77998bcca8dc54980c41e4442ad3463e.

--- StemGame/Assets/Scripts/elementPressurePlate.cs
+++ StemGame/Assets/Scripts/elementPressurePlate.cs
@@ -48,7 +48,6 @@ public class elementPressurePlate : MonoBehaviour {
 			plateOccupied = true;
 			if (other.gameObject.name.Contains(elementNeeded)) { 
 				Debug.Log("right object");
-                gameObject.GetComponent<AudioSource>().Play();
 				performAction();
                 Destroy(other.gameObject);
                 plateOccupied = false;
