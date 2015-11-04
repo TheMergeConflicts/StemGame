5986aa5789bdf3740e35bd4d80ee4782bd68e6d3
Peilin Li <lipeilin@gatech.edu>
2015-11-04 16:53:10 -0500

    UI implemented

---
+++ StemGame/Assets/LevelSelect_UIManager.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelSelect_UIManager : MonoBehaviour {
+
+	public GameObject[] levelObjects;
+
+	public void HandleLevel(int levelNum){
+		Application.LoadLevel ("Level" + levelNum);
+	}
+}
---
+++ StemGame/Assets/Scripts/UI/MainMenu_UIManager.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenu_UIManager : MonoBehaviour {
+
+	public GameObject OptionsPanel;
+	public GameObject CreditsPanel;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	public void HandleStartBtn(){
+		Debug.Log ("Start");
+		Application.LoadLevel ("LevelSelect");
+	}
+
+	public void HandleOptionsBtn(){
+		Debug.Log ("Options");
+		OptionsPanel.SetActive (true);	
+	}
+
+	public void HandleCreditsBtn(){
+		Debug.Log ("Credits");
+		CreditsPanel.SetActive (true);
+	}
+
+	public void HandleQuitBtn(){
+		Debug.Log ("Quit");
+		Application.Quit ();
+	}
+
+	public void HandleOptionsBtnBack(){
+		OptionsPanel.SetActive (false);
+	}
+
+	public void HandleCreditsBtnBack(){
+		CreditsPanel.SetActive (false);
+	}
+}
