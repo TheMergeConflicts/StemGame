1d0f874ec7d648213fcdf8aeedefe43f1cb8171c
ryanAndersen05 <ryan.andersen05@gmail.com>
2015-11-10 20:56:15 -0500

    Basic Pause menu added

---
+++ StemGame/Assets/Scripts/UI/PauseMenuLogic.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PauseMenuLogic : MonoBehaviour {
+    public GameObject pauseObject;
+    public string mainMenu = "MainMenu";
+
+    PlayerController pController;
+
+    void Start()
+    {
+        pController = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerController>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            setPauseActive();
+        }
+    }
+
+    public void setPauseActive()
+    {
+        pauseObject.SetActive(!pauseObject.activeSelf);
+        pController.enabled = !pauseObject.activeSelf;
+    }
+
+    public void quitToMainMenu()
+    {
+        Application.LoadLevel(mainMenu);
+    }
+
+    public void restartLevel()
+    {
+        Application.LoadLevel(Application.loadedLevel);
+    }
+}
