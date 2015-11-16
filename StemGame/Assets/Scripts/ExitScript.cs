99368f56de220eaf8d1461987bf06ec0e168b2cb
Garrett Stache <gstache2@gmail.com>
2015-11-16 10:57:36 -0500

    Level1 is a supremely dope tutorial

---
+++ StemGame/Assets/Scripts/ExitScript.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ExitScript : MonoBehaviour {
+    public string level;
+	
+	void OnTriggerEnter2D(Collider2D target)
+    {
+        if (target.gameObject.tag.Equals("Player")){
+            Application.LoadLevel(level);
+        }
+    }
+}
