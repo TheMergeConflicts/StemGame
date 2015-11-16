fb083643c58de7894e1fb4f198f813a8bb70f2aa
Garrett Stache <gstache2@gmail.com>
2015-11-16 08:36:37 -0500

    made a more slick level selection page

---
+++ StemGame/Assets/Scripts/LevelMapManager.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class LevelMapManager : MonoBehaviour {
+
+	enum MODE { ZONES, PANEL};
+    MODE mode = MODE.ZONES;
+    GameObject panel;
+	// Update is called once per frame
+	void Update () {
+        switch (mode)
+        {
+            case MODE.ZONES:
+                if (Input.GetMouseButtonDown(0))
+                {
+                    Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+                   
+                    RaycastHit hit;
+                    if (Physics.Raycast(ray.origin, ray.direction, out hit))
+                    {
+                        Collider coll = hit.collider;
+                        string name = coll.gameObject.name;
+                        panel = GameObject.Find(name + "Panel");
+                        panel.GetComponent<LevelPanelScale>().scaleUp();
+                        mode = MODE.PANEL;
+                    }
+                }
+                break;
+            case MODE.PANEL:
+                if (Input.GetKey(KeyCode.Escape))
+                {
+                    panel.GetComponent<LevelPanelScale>().close();
+                    mode = MODE.ZONES;
+                    panel = null;
+                }
+                break;
+        }
+	}
+}
---
+++ StemGame/Assets/Scripts/LevelPanelScale.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class LevelPanelScale : MonoBehaviour {
+    Vector2 originalSize;
+    public GameObject panel;
+	// Use this for initialization
+	void Start () {
+        originalSize = panel.GetComponent<RectTransform>().rect.size;
+        panel.GetComponent<RectTransform>().rect.size.Set(0,0);
+        panel.active = false;
+      
+    }
+	
+	public void scaleUp()
+    {
+        panel.active = true;
+        float x = panel.GetComponent<RectTransform>().rect.size.x;
+        float y = panel.GetComponent<RectTransform>().rect.size.y;
+        while (x <originalSize.x && y < originalSize.y)
+        {
+            x += Time.deltaTime;
+            y += Time.deltaTime;
+            panel.GetComponent<RectTransform>().rect.size.Set(x, y);
+        }
+    }
+
+    public void close()
+    {
+        panel.GetComponent<RectTransform>().rect.size.Set(0, 0);
+        panel.active = false;
+    }
+}
